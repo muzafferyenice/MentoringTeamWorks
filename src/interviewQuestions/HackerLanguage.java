97e4d9ebfe25a3d3ca21f617fe82000da83a1db3
MuzafferYenice <muzafferyenicet@gmail.com>
2022-04-16 02:04:56 +0300

    16/04/2022

--- src/interviewQuestions/HackerLanguage.java
+++ src/interviewQuestions/HackerLanguage.java
@@ -17,8 +17,40 @@ public class HackerLanguage {
     public static void main(String[] args) {
 
             Scanner scan = new Scanner(System.in);
-            
+            System.out.println("bir cumle giriniz");
+            String cumle=scan.nextLine().toLowerCase();
 
+            hackerDili(cumle);
+    }
+
+    private static void hackerDili(String cumle) {
+        String arr[]=new String[cumle.length()];
+
+        for (int i = 0; i < cumle.length(); i++) {
+            arr[i]=cumle.substring(i,i+1);
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i <cumle.length() ; i++) {
+            if (arr[i].contains("s")){
+                arr[i]="5";
+            }
+            if (arr[i].contains("a")){
+                arr[i]="4";
+            }
+            if (arr[i].contains("e")){
+                arr[i]="3";
+            }
+            if (arr[i].contains("i")){
+                arr[i]="2";
+            }
+            if (arr[i].contains("o")){
+                arr[i]="0";
+            }
+            System.out.print(arr[i] );
+
+        }
 
 
     }
